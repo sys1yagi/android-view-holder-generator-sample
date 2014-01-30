f12b4c82d51027e984ffdf6d147b99be6294ecdf
Fuji, Goro (gfx) <gfuji@cpan.org>
2014-01-31 07:39:26 +0900

    support custom views

--- app/src/main/java/com/example/app/generated/viewholder/FragmentMainViewHolder.java
+++ app/src/main/java/com/example/app/generated/viewholder/FragmentMainViewHolder.java
@@ -3,17 +3,19 @@ package com.example.app.generated.viewholder;
 
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import com.example.app.MyTextView;
 
 public class FragmentMainViewHolder {
 
-    public android.widget.TextView title;
-    public android.widget.TextView sub;
+    public TextView title;
+    public MyTextView sub;
     public ImageView icon;
 
     public static FragmentMainViewHolder from(View rootView) {
         FragmentMainViewHolder holder = new FragmentMainViewHolder();
-        holder.title = ((android.widget.TextView) rootView.findViewById(com.example.app.R.id.title));
-        holder.sub = ((android.widget.TextView) rootView.findViewById(com.example.app.R.id.sub));
+        holder.title = ((TextView) rootView.findViewById(com.example.app.R.id.title));
+        holder.sub = ((MyTextView) rootView.findViewById(com.example.app.R.id.sub));
         holder.icon = ((ImageView) rootView.findViewById(com.example.app.R.id.icon));
         return holder;
     }
